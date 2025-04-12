ab7fa2095361a660bc4870d74057daaef89a18ce
Aryan Chaudhary <artistaryan25@gmail.com>
2025-04-12 15:06:53 +0530

    fixed get placed students function

--- backend/controllers/get_placed_students.go
+++ backend/controllers/get_placed_students.go
@@ -11,8 +11,8 @@ import (
 	"Github.com/Aryan-2511/Placement_NIE/utils"
 )
 
-func GetPlacedStudents(w http.ResponseWriter, r *http.Request,db *sql.DB){
-	
+func GetPlacedStudents(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+
 	authHeader := r.Header.Get("Authorization")
 	if authHeader == "" {
 		http.Error(w, "Authorization token is required", http.StatusUnauthorized)
@@ -36,8 +36,8 @@ func GetPlacedStudents(w http.ResponseWriter, r *http.Request,db *sql.DB){
 		http.Error(w, "Unauthorized access", http.StatusForbidden)
 		return
 	}
-	query := `SELECT id,usn,name,email,branch,batch,company,package,placement_date,contact,placement_type FROM placed_students`
-	rows,err  := db.Query(query)
+	query := `SELECT id,usn,opportunity_id,name,email,branch,batch,company,package,placement_date,contact,placement_type FROM placed_students`
+	rows, err := db.Query(query)
 	if err != nil {
 		log.Printf("Error fetching placed students data")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -46,9 +46,9 @@ func GetPlacedStudents(w http.ResponseWriter, r *http.Request,db *sql.DB){
 	defer rows.Close()
 
 	var placedStudents []models.PlacedStudent
-	for rows.Next(){
+	for rows.Next() {
 		var student models.PlacedStudent
-		if err := rows.Scan(&student.ID, &student.USN, &student.Name, &student.Email, &student.Branch,&student.Batch, &student.Company, &student.Package, &student.PlacementDate, &student.Contact, &student.PlacementType); err != nil {
+		if err := rows.Scan(&student.ID, &student.USN, &student.OpportunityID, &student.Name, &student.Email, &student.Branch, &student.Batch, &student.Company, &student.Package, &student.PlacementDate, &student.Contact, &student.PlacementType); err != nil {
 			log.Printf("Error scanning row: %v", err)
 			http.Error(w, "Error fetching data", http.StatusInternalServerError)
 			return
@@ -59,8 +59,8 @@ func GetPlacedStudents(w http.ResponseWriter, r *http.Request,db *sql.DB){
 	json.NewEncoder(w).Encode(placedStudents)
 }
 
-func GetUnplacedStudents(w http.ResponseWriter, r *http.Request,db *sql.DB){
-	
+func GetUnplacedStudents(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+
 	authHeader := r.Header.Get("Authorization")
 	if authHeader == "" {
 		http.Error(w, "Authorization token is required", http.StatusUnauthorized)
@@ -85,7 +85,7 @@ func GetUnplacedStudents(w http.ResponseWriter, r *http.Request,db *sql.DB){
 		return
 	}
 	query := `SELECT usn,name,college_email,branch,batch,gender,contact FROM students WHERE students.isPlaced = 'NO'`
-	rows,err  := db.Query(query)
+	rows, err := db.Query(query)
 	if err != nil {
 		log.Printf("Error fetching placed students data")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -94,9 +94,9 @@ func GetUnplacedStudents(w http.ResponseWriter, r *http.Request,db *sql.DB){
 	defer rows.Close()
 
 	var UnplacedStudents []models.User
-	for rows.Next(){
+	for rows.Next() {
 		var student models.User
-		if err := rows.Scan(&student.USN, &student.Name, &student.College_Email, &student.Branch,&student.Batch,&student.Gender, &student.Contact); err != nil {
+		if err := rows.Scan(&student.USN, &student.Name, &student.College_Email, &student.Branch, &student.Batch, &student.Gender, &student.Contact); err != nil {
 			log.Printf("Error scanning row: %v", err)
 			http.Error(w, "Error fetching data", http.StatusInternalServerError)
 			return
